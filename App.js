723e86721e4fec2fd2646fa450097be172081fc3
Muhammad Irfan <107563495+MIRFAN1N@users.noreply.github.com>
2022-11-18 04:55:02 +0500

    Init

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MainNavigationTabs } from './lib/util/navigation/Tabs';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer style={AllStyles.NoBorder} >
+            <MainNavigationTabs />
+        </NavigationContainer>
+    );
+}
+
+export const AllStyles = StyleSheet.create({
+      NoBorder : {
+        borderWidth: 0,
+      }
+});
+
+export default App;
