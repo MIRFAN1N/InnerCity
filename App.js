8f3c7a26453a63d1020a8a1f9a1fe20d835a1187
Muhammad Irfan <107563495+MIRFAN1N@users.noreply.github.com>
2022-11-20 20:24:06 +0500

    Updated

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
