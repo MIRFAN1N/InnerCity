3bd1838b29e7c88f50a64ad815adfae782390eb9
Muhammad Irfan <107563495+MIRFAN1N@users.noreply.github.com>
2022-12-08 19:13:16 +0500

    updated

--- App.js
+++ App.js
@@ -3,29 +3,54 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MainNavigationTabs } from './lib/util/navigation/Tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+// import { } from './lib/screens/Intro';
+import {AppIntro} from './lib/screens/Intro';
+import {IntroScreen, HomeScreen, WelcomeScreen, RegisterScreen, LoginScreen, ProfileScreen, GamesScreen} from './lib/screens/Test/Main'
 
-const Tab = createBottomTabNavigator();
+const AuthStack = createStackNavigator();
+const MainStack = createStackNavigator();
+const Tabs = createStackNavigator();
 
-// const App = () => {
-//     return (
-//         <NavigationContainer style={AllStyles.NoBorder} >
-//             <MainNavigationTabs />
-//         </NavigationContainer>
-//     );
-// }
+const AuthStackScreen = () => {
+  return(
+    <AuthStack.Navigator screenOptions={MainNavigatorOpt}>
+      <AuthStack.Screen name="Welcome" component={WelcomeScreen} />
+      <AuthStack.Screen name="Register" component={RegisterScreen} />
+      <AuthStack.Screen name="Login" component={LoginScreen} />
+    </AuthStack.Navigator>
+  );
+}
+
+const MainStackScreen = () => {
+  return(
+    <MainStack.Navigator screenOptions={MainNavigatorOpt}>
+      <MainStack.Screen name="Home" component={HomeScreen} />
+      <MainStack.Screen name="Profile" component={ProfileScreen} />
+      <MainStack.Screen name="Games" component={GamesScreen} />
+    </MainStack.Navigator>
+  );
+}
 
 const App = () => {
-    return (
-        <View>
-            <Text>Hello</Text>
-        </View>
+    return(
+      <NavigationContainer>
+        <Tabs.Navigator  screenOptions={MainNavigatorOpt}>
+          <Tabs.Screen name='Intro' component={AppIntro} options={{tabBarVisible: false,tabBarButton: (props) => null,}}/>
+          <Tabs.Screen name='Main' component={MainStackScreen} options={{tabBarVisible: false,tabBarButton: (props) => null,}}/>
+        </Tabs.Navigator>
+      </NavigationContainer>
     );
 }
 
-export const AllStyles = StyleSheet.create({
-      NoBorder : {
-        borderWidth: 0,
-      }
-});
+export const MainNavigatorOpt = {
+  tabBarShowLabel: false,
+  headerShown: false,
+  tabBarStyle: BottomTabStyle,
+}
+
+export const BottomTabStyle = [{
+  backgroundColor : "#000"
+}];
 
 export default App;
